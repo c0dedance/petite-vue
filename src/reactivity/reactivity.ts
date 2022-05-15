8a71572a06e79168f1ea44cf705a29b2dd7acd90
c0dedance <2627702283@qq.com>
2022-05-15 22:54:48 +0800

    feat: 新增reactivity

---
+++ src/reactivity/reactivity.ts
@@ -0,0 +1,17 @@
+import { track, trigger } from './effect'
+export function reactive(raw) {
+  return new Proxy(raw, {
+    get(target, key, receiver) {
+      const res = Reflect.get(target, key)
+      // 收集依赖
+      track(target, key)
+      return res
+    },
+    set(target, key, value, receiver) {
+      const res = Reflect.set(target, key, value)
+      // 触发依赖
+      trigger(target, key)
+      return res
+    }
+  })
+}
