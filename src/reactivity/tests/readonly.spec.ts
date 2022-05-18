7c8bc7a2b9c951f3a3487d1f3c21401528852a3f
c0dedance <2627702283@qq.com>
2022-05-18 22:25:06 +0800

    test: add readonly test

---
+++ src/reactivity/tests/readonly.spec.ts
@@ -0,0 +1,20 @@
+import { readonly } from '../reactivity'
+
+describe('readonly', () => {
+  it('should make nested values readonly', () => {
+    const original = { foo: 1, bar: { baz: 2 } }
+    const wrapped = readonly(original)
+    expect(wrapped).not.toBe(original)
+    expect(wrapped.foo).toBe(1)
+  })
+  it('should call console.warn when set', () => {
+    // mock 使用假的fn去替换console.warn，方便断言
+    console.warn = jest.fn()
+    const user = readonly({
+      age: 10
+    })
+
+    user.age = 11
+    expect(console.warn).toHaveBeenCalled()
+  })
+})
