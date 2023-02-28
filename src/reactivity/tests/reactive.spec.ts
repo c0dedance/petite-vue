cae50b2f5a3ad3b8401186ed35c73010ab877a06
c0dedance <2627702283@qq.com>
2023-02-28 13:39:08 +0800

    test: add nested reactives & 深度readonly test

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -10,4 +10,17 @@ describe('reactive', () => {
     expect(isReactive(obj)).toBe(true)
     expect(isReactive(rawObj)).toBe(false)
   })
+
+  test("nested reactives", () => {
+    const original = {
+      nested: {
+        foo: 1,
+      },
+      array: [{ bar: 2 }],
+    };
+    const observed = reactive(original);
+    expect(isReactive(observed.nested)).toBe(true);
+    expect(isReactive(observed.array)).toBe(true);
+    expect(isReactive(observed.array[0])).toBe(true);
+  });
 })
--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -8,6 +8,9 @@ describe('readonly', () => {
 
     expect(isReadonly(wrapped)).toBe(true)
     expect(isReadonly(original)).toBe(false)
+    // 深度readonly
+    expect(isReadonly(wrapped.bar)).toBe(true);
+    expect(isReadonly(original.bar)).toBe(false);
 
     expect(wrapped.foo).toBe(1)
   })
