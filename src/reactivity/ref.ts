562dc65adef613a510ea2a49df51593b1cb6fb07
c0dedance <2627702283@qq.com>
2023-03-04 21:04:52 +0800

    feat: add Ref

---
+++ src/reactivity/ref.ts
@@ -0,0 +1,24 @@
+import { isTracking, trackEffects, triggerEffects } from "./effect"
+
+class RefImpl {
+  private _value: any
+  private deps = new Set()
+  constructor(value) {
+    this._value = value
+  }
+  get value() {
+    if (isTracking()) {
+      // 依赖收集
+      trackEffects(this.deps)
+    }
+    return this._value
+  }
+  set value(newVal) {
+    this._value = newVal
+    triggerEffects(this.deps)
+  }
+}
+
+export function ref(value) {
+  return new RefImpl(value)
+}
