6987e29f822ec066c845a9889d5d81ccadaeba9b
c0dedance <2627702283@qq.com>
2023-03-05 15:31:44 +0800

    feat: make nested properties reactive

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,11 +1,14 @@
-import { hasChanged } from './shared/index';
+import { hasChanged, isObject } from './shared/index';
 import { isTracking, trackEffects, triggerEffects } from "./effect"
+import { reactive } from './reactive';
 
 class RefImpl {
   private _value: any
+  private _rawValue: any
   private deps = new Set()
   constructor(value) {
-    this._value = value
+    this._rawValue = value
+    this._value = convert(value)
   }
   get value() {
     if (isTracking()) {
@@ -15,12 +18,19 @@ class RefImpl {
     return this._value
   }
   set value(newVal) {
-    if (!hasChanged(this._value, newVal)) return
-    this._value = newVal
+    if (!hasChanged(this._rawValue, newVal)) return
+    // obj.value 被赋值了新的对象，需要初始化时传入对象比较而不是和代理后的比较，否则总是不相等的
+    this._rawValue = newVal
+    this._value = convert(newVal)
     triggerEffects(this.deps)
   }
 }
 
 export function ref(value) {
   return new RefImpl(value)
+}
+
+function convert(value) {
+  // 如果.value的值对应一个对象，则走 reactive 的逻辑
+  return isObject(value) ? reactive(value) : value
 }
